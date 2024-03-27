e8a80b5865b37ac7a2cdd9cf749d8fc7a5bc092f
kei ito <ito.kei@jvckenwood.com>
2024-03-27 13:45:18 +0900

    キーボードでの選択セル選択で編集状態にならないように

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -173,12 +173,15 @@ namespace WpfApp1
 
         private void MyGrid_Selected(object sender, RoutedEventArgs e)
         {
-            // Lookup for the source to be DataGridCell
-            if (e.OriginalSource.GetType() == typeof(DataGridCell))
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
             {
-                // Starts the Edit on the row;
-                DataGrid grd = (DataGrid)sender;
-                grd.BeginEdit(e);
+                // Lookup for the source to be DataGridCell
+                if (e.OriginalSource.GetType() == typeof(DataGridCell))
+                {
+                    // Starts the Edit on the row;
+                    DataGrid grd = (DataGrid)sender;
+                    grd.BeginEdit(e);
+                }
             }
         }
 
